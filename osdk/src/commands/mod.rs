d932e57b5321a9378146ae282a84af4d77a2c7fa
zjp <jiping_zhou@foxmail.com>
2026-05-12 14:18:21 +0800

    Add `--klint` for `cargo osdk check`

--- osdk/src/commands/mod.rs
+++ osdk/src/commands/mod.rs
@@ -15,33 +15,51 @@ pub use self::{
     profile::execute_profile_command, run::execute_run_command, test::execute_test_command,
 };
 
-use crate::{arch::get_default_arch, error_msg};
+use crate::{arch::get_default_arch, cli::KtestWithForwardedArguments, error_msg};
+use std::env;
 
 /// Execute the forwarded cargo command with arguments.
-///
-/// The `cfg_ktest` parameter controls whether `cfg(ktest)` is enabled.
-pub fn execute_forwarded_command(subcommand: &str, args: &Vec<String>, cfg_ktest: bool) {
+pub fn execute_forwarded_command(subcommand: &str, cli: &KtestWithForwardedArguments) {
     let mut cargo = util::cargo();
     cargo.arg(subcommand).args(util::COMMON_CARGO_ARGS);
-    if !args.contains(&"--target".to_owned()) {
+    if !cli.args.contains(&"--target".to_owned()) {
         cargo.arg("--target").arg(get_default_arch().triple());
     }
-    cargo.args(args);
+    cargo.args(&cli.args);
 
-    let env_rustflags = std::env::var("RUSTFLAGS").unwrap_or_default();
+    let env_rustflags = env::var("RUSTFLAGS").unwrap_or_default();
     let rustflags = env_rustflags + " --check-cfg cfg(ktest)";
-    let rustflags = if cfg_ktest {
+    let rustflags = if cli.ktests {
         rustflags + " --cfg ktest"
     } else {
         rustflags
     };
-
     cargo.env("RUSTFLAGS", rustflags);
+
+    if cli.klint {
+        if subcommand != "check" {
+            error_msg!(@exit "`--klint` must be used only with `cargo osdk check`");
+        }
+        if let Ok(klint) = env::var("RUSTC") {
+            if klint.contains("klint") {
+                cargo.env("RUSTC", klint);
+            } else {
+                error_msg!(@exit "`--klint` can be used only with `cargo osdk check`");
+            }
+        } else {
+            match which::which("klint") {
+                Ok(path) => cargo.env("RUSTC", path),
+                Err(_) => {
+                    error_msg!(@exit "Command `klint` is not found. Please install it or put it in $PATH.");
+                }
+            };
+        }
+    }
 
     // When generating documentation via `cargo doc`, the `--check-cfg cfg(ktest)` flag
     // must be specified in both `RUSTFLAGS` and `RUSTDOCFLAGS`.
     if subcommand == "doc" {
-        let env_rustdocflags = std::env::var("RUSTDOCFLAGS").unwrap_or_default();
+        let env_rustdocflags = env::var("RUSTDOCFLAGS").unwrap_or_default();
         let rustdocflags = env_rustdocflags + " --check-cfg cfg(ktest)";
         cargo.env("RUSTDOCFLAGS", rustdocflags);
     }
